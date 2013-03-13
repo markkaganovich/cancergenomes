b5f552b4c446fcb2488dfe5b749483e080e01709
Mark Kaganovich <mark.kaganovich@gmail.com>
2013-03-13 11:10:18 -0400

    first commit

---
+++ cancergenomes.py
@@ -0,0 +1,42 @@
+from sqlalchemy import *
+
+db = create_engine('sqlite:///tcga.db')
+db.echo = True
+
+metadata = MetaData(db)
+
+mutations = Table('mutations', metadata,
+	Column('file', String),
+	Column('cancer', String),
+	Column('Hugo_Symbol', String),
+	Column('Entrez_Gene_Id', String),
+	Column('Center', String),
+	Column('NCBI_Build', String),
+	Column('Chromosome', String),
+	Column('Start_Position', Integer),
+	Column('End_Position', Integer),
+	Column('Variant_Classification', String), # Frame_Shift_Del, Frame_Shift_Ins, In_Frame_Del, In_Frame_Ins, Missense_Mutation, Nonsense_Mutation, Silent, Splice_Site, Translation_Start_Site, Nonstop_Mutation, 3'UTR, 3'Flank, 5'UTR, 5'Flank, IGR1 , Intron, RNA, Targeted_Region, De_novo_Start_InFrame, or De_novo_Start_OutOfFrame
+	Column('Variant_Type', String), # SNP, DNP, TNP, ONP, INS, DEL, or Consolidated
+	Column('Reference_Allele', String), # + strand ref allele
+	Column('Tumor_Seq_Allele1', String),
+	Column('Tumor_Seq_Allele2', String),
+	Column('Tumor_Sample_Barcode', String),
+	Column('Matched_Norm_Sample_Barcode', String),
+	Column('Matched_Norm_Seq_Allele1', String),
+	Column('Matched_Norm_Seq_Allele2', String),
+	Column('Tumor_Validation_Allele1', String),
+	Column('Tumor_Validation_Allele2', String),
+	Column('Matched_Norm_Validation_Allele1', String),
+	Column('Matched_Norm_Validation_Allele2', String),
+	Column('Validation_Status', String),
+	Column('Mutation_Status', String),
+	Column('Sequencer', String)
+
+
+
+
+
+
+	)
+
+mutations.create()
