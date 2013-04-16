dec1e73e6acec8b55eb896bd46b1dab816dca633
Mark Kaganovich <mark.kaganovich@gmail.com>
2013-04-16 18:07:01 -0400

    explore headers

---
+++ tcga_importer.py
@@ -0,0 +1,31 @@
+# tcga importer
+
+from sqlalchemy import *
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import and_, or_
+import sys
+import json
+import commands
+import os
+import operator
+
+import headers
+import db_importer
+
+db = create_engine('sqlite:///tcga_2.db', echo = False)
+
+metadata = MetaData(db)
+Session = sessionmaker(db)
+session = Session()
+
+directory = 'mark/cancerdata/'
+matched_files = map(lambda x: re.match(r'.maf(\Z)', x), os.listdir(directory))
+files = []
+for m in matched_files:
+    if m:
+        f = directory+m.group()
+        files.append(f)
+
+headers.explore_headers(files)
+
+
