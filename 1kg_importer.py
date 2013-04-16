9dd33bf78020050c2bcf9657b267cd8754cd0af7
Mark Kaganovich <mark.kaganovich@gmail.com>
2013-04-16 13:08:05 -0400

    script for importing 1kg files

--- 1kg_importer.py
+++ 1kg_importer.py
@@ -7,7 +7,7 @@ from sqlalchemy import create_engine, Column, String, Integer, MetaData, Table
 
 files = ['../1000GenomesData/CEU.low_coverage.2010_09.genotypes.vcf', '../1000GenomesData/YRI.low_coverage.2010_09.genotypes.vcf', '../1000GenomesData/CHBJPT.low_coverage.2010_09.genotypes.vcf', '../1000GenomesData/YRI.trio.2010_09.genotypes.vcf', '../1000GenomesData/CEU.trio.2010_09.genotypes.vcf']
 
-b = create_engine('sqlite:///GENOTYPES.db', echo = True)
+db = create_engine('sqlite:///GENOTYPES.db', echo = True)
 
 for f in files:
     print f
