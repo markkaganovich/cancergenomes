dd4cac0dd27cfe96e1aade86dcb77e2bb5307c39
Mark Kaganovich <mark.kaganovich@gmail.com>
2013-04-16 18:06:50 -0400

    explore headers

--- headers.py
+++ headers.py
@@ -8,11 +8,12 @@
 
 import csv
 
-def find_fieldnames(infile):
+def find_fieldnames(infile, not_header_flag = None):
 	reader = open(infile, 'r')
 	h = reader.next().strip('\n')
-	while(h.startswith('#')):
-		h = h[1:]
+	if not_header_flag is not None:
+		while(h.startswith(not_header_flag)): # for example '#'
+			h = h[1:]
 	dialect = csv.Sniffer().sniff(h)
 	delim = dialect.delimiter
 	fieldnames = h.split(delim)
@@ -21,10 +22,23 @@ def find_fieldnames(infile):
 
 
 def synonyms(fieldname):
+	'''
 	try:
 		col = syn[fieldname.lower()]
 	except KeyError:
 		col = fieldname.lower()
+	return col
+	'''
+	'''
+	return key in syn table that is synonyms with the given fieldname
+	'''
+	for k in syn.keys():
+		if fieldname.lower() in syn[k]:
+			col = k
+			break
+	if col is None:
+		col = fieldname.lower()
+
 	return col
 
 def get_tablename(filename):
@@ -35,10 +49,19 @@ def get_tablename(filename):
 	return tn
 
 ###############################################################################
-syn = {'chromosome': 'chrom',
-		'chr':'chrom',
-		'IID' : 'cell_line_id',
-		'id' : 'rs#'   # this is going to be an issue: ID fieldname messes with 'id' column name 
-		}
+syn = {'chrom' : ['chromosome', 'chr'],
+	'cell_line_id' : ['iid'],
+	'rs#' : ['id']
+}
 
 file_to_table = {'draft2_samples_QC+.txt': 'hapmap_draft2_samples'}
+
+def explore_headers(files):
+	out = open('explore_headers_out', 'w')
+	for f in files:
+		fieldname, delim = find_fieldnames(f)
+		for fn in fieldname:
+			out.write(fn)
+		out.write('\n')
+
+
