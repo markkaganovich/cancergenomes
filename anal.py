915e2d785a3be0c3e39fe7dbb3749c4a06bed139
Mark Kaganovich <mark.kaganovich@gmail.com>
2013-04-26 15:35:56 -0400

    added anal

---
+++ anal.py
@@ -0,0 +1,23 @@
+#anal.py
+
+from sqlalchemy.orm import backref, mapper, relation, sessionmaker
+from sqlalchemy.sql import and_, or_
+from sqlalchemy import create_engine, Column, String, Integer, MetaData, Table
+import sys
+import json
+import commands
+import os
+import csv 
+import headers
+
+
+db = create_engine('sqlite:///tcga_somatic.db', echo = False)
+
+first_pass_mutations = ['Frame_Shift_Del', 'Frame_Shift_Ins', 'Missense_Mutation', 'Nonsense_Mutation', 'Translation_Start_Site']
+
+metadata = MetaData(db)
+Session = sessionmaker(db)
+session = Session()
+
+Mutations = Table('mutations_v1', metadata, autoload = True)
+m = Mutations.select()
