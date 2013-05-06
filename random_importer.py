25fd8e6177f3da8715ff8dffc16f7ed4e2c921da
Mark Kaganovich <mark.kaganovich@gmail.com>
2013-05-06 13:23:50 -0400

    insert OV_broad

--- random_importer.py
+++ random_importer.py
@@ -10,10 +10,10 @@ import db_importer
 db = create_engine('sqlite:///tcga_somatic.db', echo = True)
 
 directory = 'mark/cancerdata/'
-filename = 'OV.braod.mit.edu_Illumina.maf '
+filename = 'OV.braod.mit.edu_Illumina.maf'
 cancer = 'OV'
 f = directory + filename
 
 primary_keys = ['chrom', 'start_position', 'tumor_sample_barcode']  
 add_columns = {'cancer_type': cancer, 'filename' : filename}
-db_importer.import_data(filename = f, tablename = 'mutations_ov', db = db, extra_columns = add_columns, key_columns = primary_keys)
+db_importer.import_data(filename = f, tablename = 'mutations_v1', db = db, extra_columns = add_columns, key_columns = primary_keys)
