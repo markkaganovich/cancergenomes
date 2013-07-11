bbef7588b5b37ff145aa0b1c0aed323ee62318b9
Mark Kaganovich <mark.kaganovich@gmail.com>
2013-07-11 12:54:20 -0400

    latest

--- entropy.py
+++ entropy.py
@@ -353,4 +353,218 @@ def table(data):
 
 
 
+tumor_suppr_rankings = []
+
+for t in tumor_suppr:
+    try:
+        tumor_suppr_rankings.append(g.index(t))
+    except:
+        continue
+
+
+tumor_suppr_mean_rankings = []
+
+g_mean = map(lambda x: x[0], min_res_by_sample_sorted)
+
+for t in tumor_suppr:
+    try:
+        tumor_suppr_mean_rankings.append(g_mean.index(t))
+    except:
+        continue
+
+oncogene_mean_rankings = []
+for o in oncogenes:
+    try:
+        oncogene_mean_rankings.append(g_mean.index(o))
+    except:
+        continue
+
+
+onco_min = map(lambda x: x[1], filter(lambda x: x[0] in oncogenes, min_res_sorted))
+tumor_min = map(lambda x: x[1], filter(lambda x: x[0] in tumor_suppr, min_res_sorted))
+
+onco_mean = map(lambda x: x[1], filter(lambda x: x[0] in oncogenes, mean_res_sorted))
+tumor_mean = map(lambda x: x[1], filter(lambda x: x[0] in tumor_suppr, mean_res_sorted))
+
+cancer_genes = []
+cancer_genes.extend(oncogenes)
+cancer_genes.extend(tumor_suppr)
+
+cancer_gene_min = filter(lambda x: x[0] in cancer_genes, min_res_sorted)
+cancer_gene_mean = filter(lambda x: x[0] in cancer_genes, mean_res_sorted)
+
+genes = poisson_by_gene.keys()
+
+sim_min = []
+sim_mean = []
+sim_inds = []
+for j in range(0, 1000):
+    random_genes = map(lambda x: genes[x], [np.random.randint(len(genes)) for i in range(0, 105)])
+    #random_min = filter(lambda x: x[0] in random_genes, min_res_sorted)
+    #r_min = np.mean(map(lambda x: x[1], random_min))
+    #random_mean = filter(lambda x: x[0] in random_genes, mean_res_sorted)
+    #r_mean = np.mean(map(lambda x: x[1], random_mean))
+    #sim_min.append(r_min)
+    #sim_mean.append(r_mean)
+    inds_rand = map(lambda x: g.index(x), filter(lambda y: y in g, random_genes))
+    sim_inds.append(inds_rand)
+
+np.mean(sim_min)
+np.mean(sim_mean)
+
+
+s = sum(map(lambda x: np.array(sorted(x)), sim_inds))
+
+
+p_s = sorted(poisson_residues.iteritems(), key = operator.itemgetter(1))
+
+ps = map(lambda x: x[0], p_s)
+setps = set(ps[0:1000]) 
+
+peak_rows = filter(lambda x: x.hugo_symbol +':'+str(x.residue['pos']) in setps, rows)
+
+
+
+
+types = list(set(map(lambda x: x.cancer_type, rows)))
+#a = sorted(poisson_residues.iteritems(), key = operator.itemgetter(1))
+#residues = map(lambda x: x[0], a)
+residue_by_type = {}
+
+singletons = filter(lambda x: x.hugo_symbol +':'+str(x.residue['pos']) not in setps, rows)
+
+
+#initialize
+res = list(setps)
+for r in res:
+    residue_by_type[r] = {}
+    for t in types:
+        residue_by_type[r][t] = 0
+
+for p in peak_rows:
+    r = p.hugo_symbol +':' + str(p.residue['pos'])
+    print r
+    if r in residues:
+        t = p.cancer_type
+        residue_by_type[r][t] = residue_by_type[r][t] + 1
+
+#general distribution of cancers
+cancer_distr = {}
+for t in types:
+    cancer_distr[t] = 0
+
+for r in rows:
+    cancer_distr[r.cancer_type] = cancer_distr[r.cancer_type] + 1
+
+total = sum(cancer_distr.values())
+for t in types:
+    cancer_distr[t] = float(cancer_distr[t])/total
+
+
+res_type_norm = {}
+for r in res:
+    res_type_norm[r] = {}
+    for t in types:
+        res_type_norm[r][t] = residue_by_type[r][t]/cancer_distr[t]
+
+
+#print to file
+output = open('residue_by_type_frac_ps_norm','w')
+
+c = '\t'
+for t in types:
+    c = c+t+','
+
+output.write(c.strip(',')+'\n')
+for r in res:
+    l = str(r) + ','
+    for t in types:
+        l = l+str(res_type_norm[r][t]) + ','
+    l = l.strip(',')
+    l = l+'\n'
+    output.write(l)
+
+single_genes_in_res = map(lambda x: x.split(':')[0], res) 
+singletongenes = map(lambda x: x.hugo_symbol, singletons)
+single_genes_in_res = filter(lambda x: x in singletongenes, single_genes_in_res)
+single_res_rows = filter(lambda x: x.hugo_symbol in single_genes_in_res, singletons)
+#single_res = map(lambda x: x.hugo_symbol + ':' + str(x.residue['pos']), single_res_rows)
+
+res.remove('WASH1:134')   #pseudogene, and it has a peak but no singletons! which is crazy
+
+single_by_type = {}
+for r in single_genes_in_res:
+    single_by_type[r] = {}
+    for t in types:
+        single_by_type[r][t] = 0.00001
+
+for p in single_res_rows:
+    r = p.hugo_symbol
+    print r
+    t = p.cancer_type
+    single_by_type[r][t] = single_by_type[r][t] + 1
+
+for r in single_by_type:
+    total = sum(single_by_type[r].values())
+    for t in types:
+        single_by_type[r][t] = single_by_type[r][t]/float(total)
+
+res_type_norm_gene = {}
+for r in res:
+    res_type_norm_gene[r] = {}
+    gene = r.split(':')[0]
+    for t in types:
+        res_type_norm_gene[r][t] = residue_by_type[r][t]/single_by_type[gene][t]
+
+
+output = open('residue_by_type_norm_gene','w')
+
+c = '\t'
+for t in types:
+    c = c+t+','
+
+output.write(c.strip(',')+'\n')
+for r in res:
+    l = str(r) + ','
+    for t in types:
+        l = l+str(res_type_norm_gene[r][t]) + ','
+    l = l.strip(',')
+    l = l+'\n'
+    output.write(l)
+
+output.close()
+
+
+
+'''
+from anal.py
+'''
+gm = sorted(genecount.iteritems(), key=operator.itemgetter(1), reverse=True)
+mutation_genes = map(lambda x: x[0], gm) 
+
+
+>>> np.mean(map(lambda x: mutation_genes.index(x), oncogenes))
+7646.3090909090906
+>>> np.mean(map(lambda x: oncos.index(x), oncogenes))         
+7764.8000000000002
+>>> np.mean(map(lambda x: mutation_genes.index(x), tumor_suppr))
+4558.3442622950815
+>>> np.mean(map(lambda x: oncos.index(x), tumor_suppr))         
+9250.0983606557384
+
+
+
+
+
+
+total = sum(single_types.values())
+single_types_frac = {}
+for t in single_types:
+    single_types_frac[t] = single_types[t]/float(total)
+
+
+
+
+
+    
 
