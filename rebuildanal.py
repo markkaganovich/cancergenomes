401a07a2d5eeaf9153f72ca547286e1699d502bf
Mark Kaganovich <mark.kaganovich@gmail.com>
2013-10-02 10:42:14 -0400

    working rebuildanal

--- rebuildanal.py
+++ rebuildanal.py
@@ -13,11 +13,10 @@ import operator
 import matplotlib.pyplot as plt
 import numpy as np
 from pysqlite2 import dbapi2 as sqlite
+import rebuildanal_helper
 
 db = create_engine('sqlite:///tcga_somatic.db', echo = False)
 metadata = MetaData(db)
-#Session = sessionmaker(db)
-#session = Session()
 
 Mutations = Table('mutations_v1', metadata, autoload = True)
 m = Mutations.select(Mutations.c.hugo_symbol != 'Unknown').execute().fetchall()   # filter out IGR variants; those that don't map to known genes
@@ -27,36 +26,27 @@ class Rows(object):
         for x in object.keys():
             setattr(self, x, object[x])
 
-rows = map(lambda x: Rows(x), m)
-gene_names = list(set(map(lambda x: x.hugo_symbol, rows)))    
+tcga_rows = map(lambda x: Rows(x), m)
+genes = list(set(map(lambda x: x.hugo_symbol, tcga_rows)))    
 
-polyphen = create_engine('sqlite:///polyphen-2.2.2-whess-2011_12.sqlite', echo = False)
-polyphen_metadata = MetaData(polyphen)
-polyphen_features = Table('features', polyphen_metadata, autoload = True)
+if 'snp_to_aa' in os.listdir('./'):
+	snp_to_aa = json.load(open('snp_to_aa'))
+else:	
+	snp_to_aa = rebuildanal_helper.get_snp_to_aa(tcga_rows)
 
-for gene in gene_names:
-	gene_rows = filter(lambda x: x.hugo_symbol == gene, rows)
-	gene_features = polyphen_features.select(and_(polyphen_features.c.gene == gene, polyphen_features.c.based_on == 'alignment')).execute().fetchall()   # filter only entries in polyphen that come from alignment not alignment_mz
-	for gr in gene_rows:
-		filter(lambda x: x.chrpos == int(gr.start_position), gene_features)
+snps = set(snp_to_aa.keys())
+tcga_residues = []
+for r in tcga_rows:
+	if r.chrom + ':' + r.start_position in snps:
+		r.residue = snp_to_aa[r.chrom + ':' + r.start_position]
+		tcga_residues.append(r)
 
-conn = sqlite.connect("SIFT/Human_CHR1.sqlite")
-tables = cursor.execute("select name from sqlite_master where type = 'table'").fetchall()
+if 'counts_aa' in os.listdir('./'):
+	counts_aa = json.load(open('counts_aa'))
+else:
+	counts_aa = rebuildanal_helper.make_counts_aa(tcga_residues)
 
-sorted_rows = sorted(rows, key=lambda x: x.chrom)
 
-def dispatch(chrom, pos):
-	pos = int(pos)
-	chrom = chrom.strip(' ')
-	db_name = 'SIFT/Human_CHR' + chrom + '.sqlite'
-	conn = sqlite.connect(db_name)
-	cursor = conn.cursor()
-	tables = map(lambda x: x[0], cursor.execute("select name from sqlite_master where type = 'table'").fetchall())
-	table_ranges = map(lambda x: (int(x.split('_')[1]), int(x.split('_')[2])), tables)
-	select_range = filter(lambda x: pos >= x[0] and pos <= x[1], table_ranges)[0]
-	select_table = 'chr' + chrom + '_' + str(select_range[0])+ '_' + str(select_range[1])
-	sift_row = cursor.execute("select AAPOS2 from " + select_table + "  where CHR == ? and COORD2 == ?", ['chr'+chrom, str(pos)]) 
-	return sift_row
 
 
 
---
+++ rebuildanal_helper.py
@@ -0,0 +1,43 @@
+rebuildanal_helper.py
+
+def get_table(chrom, pos):
+	pos = int(pos)
+	chrom = chrom.strip(' ')
+	db_name = 'SIFT/Human_CHR' + chrom + '.sqlite'
+	conn = sqlite.connect(db_name)
+	cursor = conn.cursor()
+	tables = map(lambda x: x[0], cursor.execute("select name from sqlite_master where type = 'table'").fetchall())
+	if tables is None:
+		return None
+	table_ranges = map(lambda x: (int(x.split('_')[1]), int(x.split('_')[2])), tables)
+	select_range = filter(lambda x: pos >= x[0] and pos <= x[1], table_ranges)[0]
+	select_table = 'chr' + chrom + '_' + str(select_range[0])+ '_' + str(select_range[1])
+	table = cursor.execute("select COORD2, AAPOS2 from " + select_table).fetchall()
+	table_dic = {}
+	for t in table:
+		table_dic[t[0]] = t[1]
+	positions = set(table_dic.keys())
+	return select_range, table_dic, positions
+
+
+def get_snp_to_aa(rows):
+	r = rows[1]
+	curr_chrom = str(r.chrom).strip(' ')
+	pos = int(r.start_position) 
+	current_range, current_table, positions = get_table(curr_chrom, pos)
+	for c in map(lambda x: str(x), range(1,22)) + ['X', 'Y']:
+		select_rows = filter(lambda x: x.chrom == c, rows)
+		sorted_rows = sorted(select_rows, key = lambda x: int(x.start_position))
+		print c
+		for r in sorted_rows:
+			chrom = str(r.chrom).strip(' ')
+			pos = int(r.start_position)
+			if not (pos > current_range[0] and pos < current_range[1]):
+				[current_range, current_table, positions] = get_table(chrom, pos)
+				print "changing tables"
+			if pos in positions:
+				aa = current_table[pos]
+				snp_to_aa[chrom +':' + str(pos)] = r.hugo_symbol + ':' + str(aa)
+			print chrom + ':' + str(pos)
+	return snp_to_aa
+
