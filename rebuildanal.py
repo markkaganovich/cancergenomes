52aa4b1cd95130a9c11805331d325b3b39d30d82
Mark Kaganovich <mark.kaganovich@gmail.com>
2013-10-06 14:52:34 -0400

    test threading, write to file

--- rebuildanal.py
+++ rebuildanal.py
@@ -122,7 +122,7 @@ def do_work(item):
 	print "Worker running: %s" % item
 	result = sim(item)
 	#peak_stds[item] = result
-	output.write('{0} : {1}').format(item, result)
+	output.write(str(item) + ' : ' + str(result) + '\n')
 	
 
 def worker():
