7d71dfc02d255b5d8113a7d62d92ef7e996c931d
Mark Kaganovich <mark.kaganovich@gmail.com>
2013-10-08 17:30:26 -0400

    continue simulation

--- rebuildanal.py
+++ rebuildanal.py
@@ -118,7 +118,7 @@ def sim(gene):
 	return mean_peak, std, metric
 
 
-logging.basicConfig(filename='simulations8.log',level=logging.DEBUG)
+logging.basicConfig(filename='simulations9.log',level=logging.DEBUG)
 
 
 def do_work(item):
