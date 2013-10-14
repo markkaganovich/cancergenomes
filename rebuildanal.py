1377460246f14eb9fa81f3e0e34688b5bf48fc1b
Mark Kaganovich <mark.kaganovich@gmail.com>
2013-10-14 15:52:00 -0400

    workers for chisquaure screen

--- rebuildanal.py
+++ rebuildanal.py
@@ -16,11 +16,12 @@ from pysqlite2 import dbapi2 as sqlite
 import rebuildanal_helper
 import math
 import random
-import numpy
 from collections import Counter
 import Queue
 import threading
 import logging
+import scipy
+from scipy import stats
 
 db = create_engine('sqlite:///tcga_somatic.db', echo = False)
 metadata = MetaData(db)
@@ -59,112 +60,15 @@ else:
 	json.dump(counts_aa, open('counts_aa', 'w'))
 
 
-#########################################################################################
-'''
-poission analysis of peaks per genes
+residues = list(set(map(lambda x: x.residue, tcga_residues)))
 
-'''
+tcga_residues_silent = []
+for r in tcga_silent:
+	if r.chrom + ':' + r.start_position in snps:
+		r.residue = snp_to_aa[r.chrom + ':' + r.start_position]
+		tcga_residues_silent.append(r)
 
-prtn_len = json.load(open('prtn_len'))
-
-def poisson(k, l):
-    try:
-        p = math.pow(l, k)/math.factorial(k) * np.exp(-1*l)
-    except OverflowError:
-        p = 0
-    return p
-
-'''
-poisson_residues = {}
-for g in counts_aa:
-    m = sum(counts_aa[g].values())
-    try:
-        p = float(m) / prtn_len[g]
-    except KeyError:
-        continue
-    for aa in counts_aa[g]:
-        if counts_aa[g][aa] > 1:
-            poisson_residues[g+':'+aa] = poisson(counts_aa[g][aa], p)
-
-
-avg = numpy.array([0.0] * prtn_len['BRAF'])
-'''
-'''
-peak_poisson_values = []
-while sim < 1000:
-	null = []
-	sum_mut = 0
-	for p in range(1, prtn_len['BRAF']):
-   		if sum_mut == sum(counts_aa['BRAF'].values()):
-   			break
-    	else:
-        	mut = random.randint(0, sum(counts_aa['BRAF'].values()) - sum_mut) 
-        	sum_mut += mut
-        	null.append(mut)
-	peak_poisson_values.append(map(lambda x: poisson(x, sum(null)), null))
-    sim += 1
-
-'''
-
-def sim(gene):
-	peaks_max = []
-	sim = 0
-	while sim < 1E4:
-		gene_muts = sum(counts_aa[gene].values())
-		pos = []
-		for p in range(0, gene_muts):
-			pos.append(random.randint(0, prtn_len[gene]))
-		sim +=1
-		counts = Counter(pos)
-		peaks_max.append(max(counts.values()))	
-	std = numpy.std(peaks_max)
-	mean_peak = numpy.mean(peaks_max)
-	metric = (numpy.max(counts_aa[gene].values()) - mean_peak) / std
-	return mean_peak, std, metric
-
-
-logging.basicConfig(filename='simulations.log',level=logging.DEBUG)
-
-
-def do_work(item):
-	print "Worker running: %s" % item
-	result = sim(item)
-	#peak_stds[item] = result
-	logging.info('\t' + str(item) + ' \t ' + str(result[0]) + '\t' + str(result[1]) + '\t' + str(result[2]))
-	return result
-	
-
-def worker():
-    while True:
-        item = q.get()
-        do_work(item)	
-        q.task_done()
-
-q = Queue.Queue()
-for i in range(10):
-     t = threading.Thread(target=worker)
-     t.daemon = True
-     t.start()
-
-#sim7genes = json.load(open('sim7genes'))
-#sim8genes = json.load(open('sim8genes'))
-for gene in genes:
-	if gene in counts_aa.keys() and sum(counts_aa[gene].values()) > 4 and gene in prtn_len.keys():
-		print "Queuing %s" % gene
-		q.put(gene)
-
-q.join()
-
-#json.dump(peak_stds, open('peak_stds', 'w'))
-
-'''
-
-for gene in genes:
-	if gene in counts_aa.keys() and sum(counts_aa[gene].values()) > 4:
-		result = sim(gene)
-		logging.info('\t' + str(item) + ' \t ' + str(result[0]) + '\t' + str(result[1]) + '\t' + str(result[2]))
-'''
-
+silent_residues = list(set(map(lambda x: x.residue, tcga_residues_silent)))
 
 '''
 suppr_types = map(lambda x: x.variant_classification, filter(lambda y: y.hugo_symbol in suppressors, tcga_rows))
@@ -181,27 +85,136 @@ onco_counter = Counter(onco_types)
 
 ## read in simulations.log result
 
-'''
-sim_gene_results = {}
-lines = open('simulation.log.copy').readlines()
-simgenes = map(lambda x: x.split('\t')[1].strip('  '), lines)
-for gene in simgenes:
-	sim_gene_results[gene] = {}
+def sim_output(simulation_file = 'simulations.log'):
 
-for l in lines:
-	tabs = l.split('\t')
-	g = tabs[1].strip('  ')
-	sim_gene_results[g]= {}
-	sim_gene_results[g]['mean'] = float(tabs[2])
-	sim_gene_results[g]['std'] = float(tabs[3])
-	sim_gene_results[g]['metric'] = float(tabs[4].strip('\n'))
+	sim_gene_results = {}
+	lines = open(simulation_file).readlines()
+	simgenes = map(lambda x: x.split('\t')[1].strip('  '), lines)
+	for gene in simgenes:
+		sim_gene_results[gene] = {}
 
-'''
+	for l in lines:
+		tabs = l.split('\t')
+		g = tabs[1].strip('  ')
+		sim_gene_results[g]= {}
+		sim_gene_results[g]['mean'] = float(tabs[2])
+		sim_gene_results[g]['std'] = float(tabs[3])
+		sim_gene_results[g]['metric'] = float(tabs[4].strip('\n'))
 
+	return sim_gene_results
 
 ## per residue
 
-for gene in tcga_residues:
-	mean = sim_gene_results[gene]['mean']
-	std = sim_gene_results[gene]['std']
-	counts_aa[gene]
+def pile_up(sim_gene_results, residues, counts_aa):
+	pile_ups = {}
+	what = []
+	for res in residues:
+		gene = res.split(':')[0]
+		try:
+			mean = sim_gene_results[gene]['mean']
+			std = sim_gene_results[gene]['std']
+		except KeyError:
+			continue
+		if res not in counts_aa[gene].keys():
+			what.append(res)
+		else:
+			val = counts_aa[gene][res]
+			pile_ups[res] = (val-mean)/std
+
+	return pile_ups
+
+
+sim_gene_results = sim_output('simulations.log')
+pile_ups = pile_up(sim_gene_results, residues, counts_aa)
+pileupsorted = sorted(pile_ups.iteritems(), key=operator.itemgetter(1), reverse=True)
+
+
+counts_aa_silent = json.load(open('counts_aa_silent'))
+sim_gene_results_silent = sim_output('simulations_silent.log')
+pile_up_silent = pile_up(sim_gene_results_silent, silent_residues, counts_aa_silent) 
+
+##########################################################################
+# cancer type distribution
+
+cancer_types = map(lambda x: x.cancer_type, tcga_residues)
+cancer_types_silent = map(lambda x: x.cancer_type, tcga_residues_silent)
+cancer_types_counts = Counter(cancer_types)
+cancer_types_silent_count = Counter(cancer_types_silent)
+map(lambda x: (x, cancer_types_counts[x]/float(sum(cancer_types_counts.values()))), cancer_types_counts.keys())
+map(lambda x: (x, cancer_types_silent_count[x]/float(sum(cancer_types_silent_count.values()))), cancer_types_silent_count.keys())
+
+cancers = cancer_types_counts.keys()
+total = float(sum(cpc.values()))
+distr = []
+for c in cancers:	
+	if c in cpc.keys():
+		distr.append(cpc[c]/total)
+	else:
+		distr.append(0.0)
+	
+def get_np_array(cancers, cancer_counts):
+	distr = []
+	for c in cancers:	
+		if c in cancer_counts.keys():
+			distr.append(float(cancer_counts[c]))
+		else:
+			distr.append(0.0)
+	distr = np.array(distr)
+	#distr = distr/sum(distr)
+	return distr
+
+overall = get_np_array(cancers, cancer_types_counts)
+silent_distr = get_np_array(cancers, cancer_types_silent_count)
+
+scipy.stats.ks_2samp(overall, silent_distr)
+
+
+expected_freq = overall / sum(overall) 
+def peak_chisq(peak, tcga_residues, expected_freq):
+	peak_rows = filter(lambda x: x.residue == peak[0], tcga_residues)
+	peak_cancers = Counter(map(lambda x: x.cancer_type, peak_rows))
+	distr = get_np_array(cancers, peak_cancers) 
+	chisq = scipy.stats.chisquare(distr, expected_freq*sum(distr))
+	return chisq
+
+#distr_ls = []
+chisq = []
+for peak in pileupsorted:
+	peak_rows = filter(lambda x: x.residue == peak[0], tcga_residues)
+	peak_cancers = Counter(map(lambda x: x.cancer_type, peak_rows))
+	distr = get_np_array(cancers, peak_cancers) 
+	chisq.append(scipy.stats.chisquare(distr, expected_freq*sum(distr)))
+
+
+
+logging.basicConfig(filename='pile_up_chisq.log',level=logging.DEBUG)
+
+def do_work(peak):
+	print "Worker running: %s" % item
+	result = peak_chisq(peak)
+	logging.info('\t' + str(item) + ' \t ' + str(result[0]) + '\t' + str(result[1]))
+	return result
+	
+
+def worker():
+    while True:
+        peak = q.get()
+        do_work(peak)	
+        q.task_done()
+
+q = Queue.Queue()
+for i in range(14):
+     t = threading.Thread(target=worker)
+     t.daemon = True
+     t.start()
+
+for peak in pileupsorted:
+	print "Queuing %s" % peak[0]
+	q.put(peak)
+
+q.join()
+
+
+
+
+
